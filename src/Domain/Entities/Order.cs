4a2340536c415dda365629317c61dba906366dc4
Nicolas Cataldi <nicolascataldi1@gmail.com>
2024-06-18 23:35:19 -0300

    Client and Order classes

---
+++ src/Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Order // add Client heritance
+    {
+        public int Id { get; set; }
+
+        public Order(int id) // add Client heritance
+        {
+            Id = id;
+        }
+    }
+}
