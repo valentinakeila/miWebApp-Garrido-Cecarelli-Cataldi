ac5631c66b31fea86e1117c0beea4ad3623a2d24
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-12 14:28:44 -0300

    renewed content in Domain/Enums

---
+++ src/Domain/Enums/OrderState.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain.Enums
+{
+    public enum OrderState
+    {
+        Pendent,
+        Completed,
+        Canceled
+    }
+}
---
+++ src/Domain/Enums/UserRole.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain.Enums
+{
+    public enum UserRole
+    {
+        Client,
+        Admin,
+        SuperAdmin
+    }
+}
