fd31448aa22d6a5078827f8be5354d84fb40d0f6
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-12 16:49:12 -0300

    we renew the content in the presentation/web layer

---
+++ src/Web/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Application.Models.Request;
+using Domain.Enums;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<List<UserDto?>> GetAllUsers()
+        {
+            return _userService.GetAllUsers();
+        }
+
+        [HttpGet("[action]/{id}")]
+        public ActionResult<UserDto?> GetUserById([FromRoute] int id)
+        {
+            return _userService.GetUserById(id);
+        }
+
+        [HttpGet("[action]/{role}")]
+        public ActionResult<List<UserDto>> GetUsersByRole([FromRoute] UserRole role)
+        {
+            return _userService.GetUsersByRole(role);
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult<UserDto> CreateNewUser([FromBody] UserCreateRequest userCreateRequest)
+        {
+            return _userService.CreateNewUser(userCreateRequest);
+        }
+
+        [HttpPut("[action]/{id}")]
+        public ActionResult ModifyUserData([FromRoute] int id, [FromBody] UserUpdateRequest userUpdateRequest)
+        {
+            _userService.ModifyUserData(id, userUpdateRequest);
+            return Ok();
+        }
+
+        [HttpDelete("[action]/{id}")]
+        public ActionResult DeleteUser([FromRoute] int id)
+        {
+            _userService.DeleteUser(id);
+            return Ok();
+        }
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.Data.Sqlite;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Domain.Interfaces;
+using Domain.Entities;
+using Application.Interfaces;
+using Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Configure the SQLite connecition
+string connectionString = builder.Configuration["ConnectionStrings:duffeeWebAppDBConnectionString"]!;
+
+var connection = new SqliteConnection(connectionString);
+connection.Open();
+
+// Set journal mode to DELETE using PRAGMA statement
+using (var command = connection.CreateCommand())
+{
+    command.CommandText = "PRAGMA journal_mode = DELETE;";
+    command.ExecuteNonQuery();
+}
+
+builder.Services.AddDbContext<ApplicationContext>(dbContextOptions => dbContextOptions.UseSqlite(connection));
+
+#region Repositories
+builder.Services.AddScoped<IRepositoryBase<User>, EfRepository<User>>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+#endregion
+
+#region Services
+builder.Services.AddScoped<IUserService, UserService>();
+#endregion
 
 var app = builder.Build();
 
