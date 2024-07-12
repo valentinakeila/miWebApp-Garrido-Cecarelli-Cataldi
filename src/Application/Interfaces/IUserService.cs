f5cf8267d7c82011daa32bcaa8d29bf8838aaea1
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-12 16:23:44 -0300

    we renew the content in the application layer

---
+++ src/Application/Interfaces/IUserService.cs
@@ -0,0 +1,26 @@
+﻿using Application.Models;
+using Application.Models.Request;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUserService
+    {
+        UserDto? GetUserById(int id);
+
+        List<UserDto?> GetAllUsers();
+
+        UserDto CreateNewUser(UserCreateRequest userCreateRequest);
+
+        void ModifyUserData(int id, UserUpdateRequest userUpdateRequest);
+
+        void DeleteUser(int id);
+
+        List<UserDto> GetUsersByRole(UserRole role);
+    }
+}
---
+++ src/Application/Models/UserDto.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+
+        public static UserDto Create(User user)
+        {
+            var dto = new UserDto();
+            dto.Id = user.Id;
+            dto.FirstName = user.FirstName;
+            dto.LastName = user.LastName;
+            dto.Email = user.Email;
+            dto.Role = user.Role;
+
+            return dto;
+        }
+
+        public static List<UserDto?> CreateList(IEnumerable<User> users)
+        {
+            List<UserDto?> listDto = [];
+
+            foreach (var u in users)
+            {
+                listDto.Add(Create(u));
+            }
+
+            return listDto;
+        }
+    }
+}
+
+// public string CompleteName { get => Name + " " + LastName; }
