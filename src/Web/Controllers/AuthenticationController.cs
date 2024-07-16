9c87e654b90878d5e36ff49ebee66a95e012c8ed
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-16 16:01:03 -0300

    we create the AuthenticationController

---
+++ src/Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Application.Models.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ICustomAuthenticationService _customAuthenticationService;
+
+        public AuthenticationController(IConfiguration config, ICustomAuthenticationService authenticationService)
+        {
+            _config = config;
+            _customAuthenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Authenticate([FromBody] AuthenticationRequest authenticationRequest)
+        {
+            string token = _customAuthenticationService.Authenticate(authenticationRequest);
+
+            return Ok(token);
+        }
+    }
+}
