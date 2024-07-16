0b91ed0ed73d62ae3fc813973fb29e97cb3344a4
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-07-16 14:53:49 -0300

    We create the nested AuthenticationServiceOptions class

---
+++ src/Infrastructure/Services/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class AuthenticationService
+    {
+        public class AuthenticationServiceOptions
+        {
+            public const string AutenticacionService = "AutenticacionService";
+
+            public string Issuer { get; set; }
+            public string Audience { get; set; }
+            public string SecretForKey { get; set; }
+        }
+    }
+}
